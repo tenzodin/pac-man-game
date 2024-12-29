7bcc4342781bbc61be9fa2bdfacfadf573c4bc18
tenzodin main <140925631+tenzodin@users.noreply.github.com>
2024-12-29 17:15:50 -0500

    Init Game Layout

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from board import boards
 import pygame
 
 pygame.init()
@@ -8,13 +9,30 @@ screen = pygame.display.set_mode([WIDTH, HEIGHT])
 timer = pygame.time.Clock()
 fps = 60
 font = pygame.font.Font('freesansbold.ttf', 20)
-
+level = boards
 pygame.display.set_caption('Pac Man Game') 
+color = 'blue'
+
+def draw_board():
+    num1 = ((HEIGHT - 50) // 32)
+    num2 = (WIDTH // 30)
+    for i in range(len(level)):
+        for j in range(len(level[i])):
+            if level[i][j] == 1:
+                pygame.draw.circle(screen, 'white', (j * num2 + (0.5 * num2), i * num1 + (0.5 * num1)), 4)
+            if level[i][j] == 2:
+                pygame.draw.circle(screen, 'white', (j * num2 + (0.5 * num2), i * num1 + (0.5 * num1)), 10)
+            if level[i][j] == 3:
+                pygame.draw.line(screen, color, (j * num2 + (0.5 * num2), i * num1),
+                                 (j * num2 + (0.5 * num2), i * num1))
+
+
 
 run = True
 while run:
     timer.tick(fps)
     screen.fill('black')
+    draw_board()
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:

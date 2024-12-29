5287dbed41d656038c9281343554f67562ea9202
tenzodin main <140925631+tenzodin@users.noreply.github.com>
2024-12-29 18:08:45 -0500

    Map Layout Completed!

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from board import boards
 import pygame
+import math
 
 pygame.init()
 
@@ -12,6 +13,7 @@ font = pygame.font.Font('freesansbold.ttf', 20)
 level = boards
 pygame.display.set_caption('Pac Man Game') 
 color = 'blue'
+PI = math.pi
 
 def draw_board():
     num1 = ((HEIGHT - 50) // 32)
@@ -24,7 +26,25 @@ def draw_board():
                 pygame.draw.circle(screen, 'white', (j * num2 + (0.5 * num2), i * num1 + (0.5 * num1)), 10)
             if level[i][j] == 3:
                 pygame.draw.line(screen, color, (j * num2 + (0.5 * num2), i * num1),
-                                 (j * num2 + (0.5 * num2), i * num1))
+                                 (j * num2 + (0.5 * num2), i * num1 + num1), 3)
+            if level[i][j] == 4:
+                pygame.draw.line(screen, color, (j * num2, i * num1 + (0.5 * num1)),
+                                 (j * num2 + num2, i * num1 + (0.5 * num1)), 3)
+            if level[i][j] == 5:
+                pygame.draw.arc(screen, color, [(j*num2 - (num2 * 0.4)) - 2, (i * num1 + (0.5 * num1)),
+                                                num2, num1], 0, PI / 2, 3)
+            if level[i][j] == 6:
+                pygame.draw.arc(screen, color, [(j*num2 + (num2 * 0.5)), (i * num1 + (0.5 * num1)),
+                                                num2, num1], PI / 2, PI,  3)
+            if level[i][j] == 7:
+                pygame.draw.arc(screen, color, [(j*num2 + (num2 * 0.5)), (i * num1 - (0.4 * num1)),
+                                                num2, num1], PI, 3*PI / 2, 3)
+            if level[i][j] == 8:
+                pygame.draw.arc(screen, color, [(j*num2 - (num2 * 0.4)), (i * num1 - (0.4 * num1)),
+                                                num2, num1], 3* PI / 2, 2 * PI, 3)
+            if level[i][j] == 9:
+                pygame.draw.line(screen, 'white', (j * num2, i * num1 + (0.5 * num1)),
+                                 (j * num2 + num2, i * num1 + (0.5 * num1)), 3)
 
 
 

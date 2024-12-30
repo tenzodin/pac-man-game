e6c68d6070b4be29d8ee888d3fcd17b6c923b213
tenzodin main <140925631+tenzodin@users.noreply.github.com>
2024-12-29 19:01:35 -0500

    Drew the player and added left,right,up,down functionality

--- main.py
+++ main.py
@@ -14,6 +14,14 @@ level = boards
 pygame.display.set_caption('Pac Man Game') 
 color = 'blue'
 PI = math.pi
+player_images = []
+for i in range(1, 5):
+    player_images.append(pygame.transform.scale(pygame.image.load(f'pac-man-game/player_images/{i}.png'), (45,45)))
+
+player_x = 450
+player_y = 663
+direction = 0
+counter = 0
 
 def draw_board():
     num1 = ((HEIGHT - 50) // 32)
@@ -46,17 +54,44 @@ def draw_board():
                 pygame.draw.line(screen, 'white', (j * num2, i * num1 + (0.5 * num1)),
                                  (j * num2 + num2, i * num1 + (0.5 * num1)), 3)
 
-
+def draw_player():
+    #0:RIGHT, 1: LEFT, 2: UP, 3: DOWN
+    if direction == 0:
+        screen.blit(player_images[counter // 5] , (player_x, player_y))
+    elif direction == 1:
+        screen.blit(pygame.transform.flip(player_images[counter // 5], True, False) , (player_x, player_y))
+    elif direction == 2:
+        screen.blit(pygame.transform.rotate(player_images[counter // 5], 90) , (player_x, player_y))
+    elif direction == 3:
+        screen.blit(pygame.transform.rotate(player_images[counter // 5], 270) , (player_x, player_y))
 
 run = True
 while run:
     timer.tick(fps)
+    if counter < 19:
+        counter += 1
+    else:
+        counter = 0
+
+
+
     screen.fill('black')
     draw_board()
+    draw_player()
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                direction = 0
+            if event.key == pygame.K_LEFT:
+                direction = 1
+            if event.key == pygame.K_UP:
+                direction = 2
+            if event.key == pygame.K_DOWN:
+                direction = 3
+            
 
     pygame.display.flip()
 
